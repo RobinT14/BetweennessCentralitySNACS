1029606c366346f9e24de2c09ea002915db0c4c7
RT <robin.the@xs4all.nl>
2023-12-17 17:43:05 +0100

    Update betweenness.py

--- Code/betweenness.py
+++ Code/betweenness.py
@@ -97,7 +97,7 @@ def perform_experiments(console, graph, input_file):
         for i in range(0, 1):
             start_time_kadabra = time.time()
             betweenness_kadabra = nk.centrality.KadabraBetweenness(
-                G, 0.0001, 0.8)  # these are the default settings
+                G, 0.1, 0.8)  # these are the default settings
             betweenness_kadabra.run()
             end_time_kadabra = time.time()
             average_time_kadabra += (end_time_kadabra -
